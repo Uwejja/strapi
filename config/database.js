a9eb64da265f3c044404aac608dfc36441944247
Vegros <web@uwejja.com>
2023-07-14 10:55:03 +0200

    commit-4

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '0.0.0.0'),
+      host: env('PGHOST', 'containers-us-west-117.railway.app'),
       port: env.int('PGPORT',6134),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'sc0QEBgh8izCdpbZ6Ae4'),
       ssl: env.bool(true),
     },
     pool: { min: 0 }
